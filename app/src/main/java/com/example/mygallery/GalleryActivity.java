b1fd8cc1ffc020ecf451b35c45f38dab0adb0515
Denis Cherepanov <cherepanov.denis94@mail.ru>
2017-07-02 13:54:22 +0300

    load data in AsynTask class

--- app/src/main/java/com/example/mygallery/GalleryActivity.java
+++ app/src/main/java/com/example/mygallery/GalleryActivity.java
@@ -1,5 +1,6 @@
 package com.example.mygallery;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -63,6 +64,19 @@ public class GalleryActivity extends AppCompatActivity {
         @Override
         public int getItemCount() {
             return galleryItems.size();
+        }
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryItem>>{
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... voids) {
+            return new FlickFetcher().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            mItems = items;
         }
     }
 }
